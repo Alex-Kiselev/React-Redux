c6ad63c8e8fef2804cdba33625d5ca492701f33c
AA_KISELEV1 <>
2024-01-13 14:05:47 +0300

    4) Jsx и создание через React.CreateElement
    1ый вариант как бы выглядела наша разметка вместо jsx вариант

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,17 @@
+import React from 'react';
 import './App.css';
 
 function App() {
 
-  return (
-    <>
-      <div>Начало проект</div>
-    </>
-  );
+  return React.createElement('div', {}, React.createElement('h1', {}, 'Заголовок', React.createElement('div', {}, 'Начало проекта')));
+
+  // return (
+  //   <div>
+  //     <h1>Загаловок</h1>
+  //     <div>Начало проект</div>
+  //   </div>
+  // );
+
 }
 
 export default App;
