ff6d3b555428533c4a65f40d4a45e5d477d3e953
AA_KISELEV1 <>
2024-01-14 14:44:59 +0300

    3.11. Упражнение - Вывод даты
    Добавление интернационализация ДЛЯ даты

--- vite-project/src/components/JournalItem/JournalItem.jsx
+++ vite-project/src/components/JournalItem/JournalItem.jsx
@@ -2,15 +2,15 @@ import './JournalItem.css';
 
 
 function JournalItem({ title, text, date }) {
-    // const title = 'Подготовка к обновлению курсов';
-    // const date = new Date();
-    // const text = 'Горные походы открывают удивительные природные ландшафты';
+    const formatedDate = new Intl.DateTimeFormat('ru-Ru').format(date);
+
+
 
     return (
         <div className='journal-item'>
             <h2 className='journal-item__header'>{title}</h2>
             <h2 className='journal-item__body'>
-                <div className='journal-item__date'>{date.toString()}</div>
+                <div className='journal-item__date'>{formatedDate}</div>
                 <div className='journal-item__text'>{text}</div>
             </h2>
         </div>
