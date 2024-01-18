1f43200ff9e5dcd28c7db8bc7e6cb0386ade5b4b
AA_KISELEV1 <>
2024-01-18 20:48:29 +0300

    5.4. Отображение по условию
    1) 1ый вариант с присвоением в let переменную
    2) вынести полностью логику отображения в компонент

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css';
-import JournalItem from './components/JournalItem/JournalItem';
-import CardButton from './components/CardButton/CardButton';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 import Body from './layouts/Body/Body';
 import Header from './components/Header/Header';
@@ -13,18 +11,18 @@ function App() {
 
 	// return React.createElement('div', {}, React.createElement('h1', {}, 'Заголовок', React.createElement('div', {}, 'Начало проекта')));
 	const INITIAL_DATA = [
-		{
-			id: 1,
-			title: 'Подготовка к обновлению курсов',
-			date: new Date(),
-			text: 'Горные походы открывают удивительные природные ландшафты'
-		},
-		{
-			id: 2,
-			title: 'Поход в годы',
-			date: new Date(),
-			text: 'Думал, что очень много времени'
-		}
+		// {
+		// 	id: 1,
+		// 	title: 'Подготовка к обновлению курсов',
+		// 	date: new Date(),
+		// 	text: 'Горные походы открывают удивительные природные ландшафты'
+		// },
+		// {
+		// 	id: 2,
+		// 	title: 'Поход в годы',
+		// 	date: new Date(),
+		// 	text: 'Думал, что очень много времени'
+		// }
 	];
 	const [items, setItems] = useState(INITIAL_DATA);
 	console.log('🚀 ~ App ~ items:', items);
@@ -39,32 +37,46 @@ function App() {
 		}]);
 	};
 
-	const sortItems = (a, b) => {
-		if (a.date < b.date) {
-			return 1;
-		} else {
-			return -1;
-		}
 
-	};
 
+
+
+	// const sortItems = (a, b) => {
+	// 	if (a.date < b.date) {
+	// 		return 1;
+	// 	} else {
+	// 		return -1;
+	// 	}
+
+	// };
+
+	// // 1ыый -  Хороший способ
+	// let list = <p>Записей нет, добавьте первую запись</p>;
+	// if (items.length > 0) {
+	// 	list = items.sort(sortItems).map(el => (
+	// 		<CardButton key={el.id}>
+	// 			<JournalItem
+	// 				title={el.title}
+	// 				text={el.text}
+	// 				date={el.date}
+	// 			/>
+	// 		</CardButton>
+	// 	));
+	// }
 
 	return (
 		<div className='app'>
 			<LeftPanel>
 				<Header />
 				<JournaAddButton />
-				<JournaList>
-					{items.sort(sortItems).map(el => (
-						<CardButton key={el.id}>
-							<JournalItem
-								title={el.title}
-								text={el.text}
-								date={el.date}
-							/>
-						</CardButton>
-					))}
-				</JournaList>
+
+
+				{/* 1 ВАРИАНТ либо 2 вариант перенести логику полностью */}
+				{/* <JournaList>
+					{list} 
+				</JournaList> */}
+
+				<JournaList items={items}/>
 			</LeftPanel>
 			<Body>
 				<JournalForm onSubmit={addTodo} />
--- vite-project/src/components/JournaList/JournaList.jsx
+++ vite-project/src/components/JournaList/JournaList.jsx
@@ -1,15 +1,37 @@
+import CardButton from '../CardButton/CardButton';
+import JournalItem from '../JournalItem/JournalItem';
 import './JournaList.css';
 
 
-function JournaList({ children }) {
+function JournaList({ items }) {
 
+	if (items.length === 0) {
+		<p>Записей нет, добавьте первую запись</p>;
+	}
 
-	return (
-		<div className="journal-list">
-			{children}
-		</div>
-	);
+	const sortItems = (a, b) => {
+		if (a.date < b.date) {
+			return 1;
+		} else {
+			return -1;
+		}
+
+	};
+
+	// 2- подход разноса логика
+	return <div className="journal-list">
+		{items.sort(sortItems).map(el => (
+			<CardButton key={el.id}>
+				<JournalItem
+					title={el.title}
+					text={el.text}
+					date={el.date}
+				/>
+			</CardButton>
+		))}
+	</div>;
 }
+
 
 
 export default JournaList;
