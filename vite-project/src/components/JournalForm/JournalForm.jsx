5e0d1a637b711f82661dfbec127807bd30f7d4a0
AA_KISELEV1 <>
2024-01-18 21:36:29 +0300

    6.1. Изменение стилей
    Работа с валидацией и стилями

--- vite-project/src/components/JournalForm/JournalForm.jsx
+++ vite-project/src/components/JournalForm/JournalForm.jsx
@@ -1,8 +1,16 @@
 import './JournalForm.css';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
 
-function JournalForm({onSubmit}) {
+function JournalForm({ onSubmit }) {
+
+	const [formValidState, setFormValidState] = useState({
+		title: true,
+		post: true,
+		date: true
+	});
+
 	// const [inputData, setInputData] = useState('');
 	// const [state, setState] = useState({
 	// 	age: 31
@@ -23,6 +31,31 @@ function JournalForm({onSubmit}) {
 
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
+
+
+		let isFormValid = true;
+		if (!formProps.title?.trim().length) {
+			setFormValidState(prevState => ({ ...prevState, title: false }));
+			isFormValid = false;
+		} else {
+			setFormValidState(prevState => ({ ...prevState, title: true }));
+		}
+		if (!formProps.post?.trim().length) {
+			setFormValidState(prevState => ({ ...prevState, post: false }));
+			isFormValid = false;
+		} else {
+			setFormValidState(prevState => ({ ...prevState, post: true }));
+
+		}
+		if (!formProps.date) {
+			setFormValidState(prevState => ({ ...prevState, date: false }));
+			isFormValid = false;
+		} else {
+			setFormValidState(prevState => ({ ...prevState, date: true }));
+		}
+		if (!isFormValid) {
+			return;
+		}
 		onSubmit(formProps);
 
 
@@ -37,10 +70,10 @@ function JournalForm({onSubmit}) {
 
 	return (
 		<form className='journal-form' onSubmit={addJournalItem}>
-			<input type='text' name='title' />
-			<input type='date' name='date' />
-			<input type='text' name='tag'  />
-			<textarea name="text" id="" cols="30" rows="10"></textarea>
+			<input type='text' name='title' style={{ border: formValidState.title ? undefined : '2px solid red' }} />
+			<input type='date' name='date' style={{ border: formValidState.date ? undefined : '2px solid red' }} />
+			<input type='text' name='tag' />
+			<textarea name="post" id="" cols="30" rows="10" style={{ border: formValidState.post ? undefined : '2px solid red' }} />
 			<Button text="Сохранить" onClickCustom={() => {
 				// console.log('Нажали');
 			}} />
