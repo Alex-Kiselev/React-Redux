cdce4586417cde7b90504fb031c7088f27401a49
AA_KISELEV1 <>
2024-01-21 00:30:54 +0300

    6.2. Динамические классы без ЛИБ

--- vite-project/src/components/JournalForm/JournalForm.jsx
+++ vite-project/src/components/JournalForm/JournalForm.jsx
@@ -70,10 +70,10 @@ function JournalForm({ onSubmit }) {
 
 	return (
 		<form className='journal-form' onSubmit={addJournalItem}>
-			<input type='text' name='title' style={{ border: formValidState.title ? undefined : '2px solid red' }} />
-			<input type='date' name='date' style={{ border: formValidState.date ? undefined : '2px solid red' }} />
+			<input type='text' name='title' className={`input ${formValidState.title ? '' : 'invalid'}`}/>
+			<input type='date' name='date' className={`input ${formValidState.date ? '' : 'invalid'}`}/>
 			<input type='text' name='tag' />
-			<textarea name="post" id="" cols="30" rows="10" style={{ border: formValidState.post ? undefined : '2px solid red' }} />
+			<textarea name="post" id="" cols="30" rows="10"  className={`input ${formValidState.post ? '' : 'invalid'}`}/>
 			<Button text="Сохранить" onClickCustom={() => {
 				// console.log('Нажали');
 			}} />
